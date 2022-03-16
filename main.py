dbca79a2cf3849310da1d27bc90d4487622223fd
qyang <gnaymie@gmail.com>
2022-03-16 16:42:06 -0400

    add .env package to allow app run locally

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
